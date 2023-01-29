f62d82e0e6b95039a1f0e07723c515a362aa713d
Saurabh Karn <saurabhkarn007@gmail.com>
2023-01-29 22:27:49 +0530

    Completed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://purunaik:purunaik@cluster0.zgxxxk0.mongodb.net/course-management-system", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://SAURABH:Soa4GdK4yRvlVN5i@cluster0.umtgp.mongodb.net/Course-Management-System", {useNewUrlParser: true})
 .then(()=>console.log("Connected to mongoose..."))
 .catch((err)=>console.log(err));
 
